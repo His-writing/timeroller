db63e072599ad668640f0b9946ecfcd055d40013
shuzhenguo <shuzhen.guo@bj.china.com>
2017-06-19 15:36:23 +0800

    前3周，后一周，凑够30天滚轮😄

--- timeroller/ViewController.m
+++ timeroller/ViewController.m
@@ -13,70 +13,75 @@
 
 @interface ViewController ()
 {
-    //日期存储数组
-    //年数组
-    NSMutableArray *_yearArray;
-    
-    //月数组
-    NSMutableArray *_monthArray;
-    
-    NSMutableArray *_monthArray1;
-    //天数组
-    NSMutableArray *_dayArray;
-    
-    //周和天
-    NSMutableArray *_dayWeeksArray;
-    
-    //小时的数组
-    NSMutableArray *_hourArray;
-    //分钟的数组
-    NSMutableArray *_minuteArray;
-    
-    //限制model
-    UUDatePicker_DateModel *_maxDateModel;
-    UUDatePicker_DateModel *_minDateModel;
-    
-    //记录位置
-    NSInteger _yearIndex;
-    NSInteger _monthIndex;
+//    //日期存储数组
+//    //年数组
+//    NSMutableArray *_yearArray;
+//    
+//    //月数组
+//    NSMutableArray *_monthArray;
+//    
+//    NSMutableArray *_monthArray1;
+//    //天数组
+//    NSMutableArray *_dayArray;
+//    
+//    //周和天
+//    NSMutableArray *_dayWeeksArray;
+//    
+//    //小时的数组
+//    NSMutableArray *_hourArray;
+//    //分钟的数组
+//    NSMutableArray *_minuteArray;
+//    
+//    //限制model
+//    UUDatePicker_DateModel *_maxDateModel;
+//    UUDatePicker_DateModel *_minDateModel;
+//    
+//    //记录位置
+//    NSInteger _yearIndex;
+//    NSInteger _monthIndex;
     NSInteger _dayIndex;
-    NSInteger _hourIndex;
-    NSInteger _minuteIndex;
-    
-    
-    int _pageNum;
-    
-    
+//    NSInteger _hourIndex;
+//    NSInteger _minuteIndex;
+//    
+//    
+//    int _pageNum;
+//    
+//    
 }
 //日期存储数组
 
 //年数组
-@property(strong,nonatomic) NSMutableArray *yearArray;
+//@property(strong,nonatomic) NSMutableArray *yearArray;
 
 //@property(nonnull,strong)UIButton *  zkSegment1LeftButton;
 
 @property (nonatomic,assign)ZKSegmentStyle zkSegmentStyle;
 
-//月数组
 
-@property(strong,nonatomic) NSMutableArray *monthArray;
-
-//天数组
-@property(strong,nonatomic) NSMutableArray *dayArray;
+@property(nonatomic,strong)NSMutableArray *dataArray ;
+@property(nonatomic,strong)NSMutableArray *YearMonthDayArray ;
 
 
-//周和天
-@property(strong,nonatomic) NSMutableArray *dayWeeksArray;
-
-
-//小时的数组
-@property(strong,nonatomic) NSMutableArray *hourArray;
-
-//分钟的数组
-@property(strong,nonatomic) NSMutableArray *minuteArray;
-
-@property(strong,nonatomic) ArchivesModel* achiverDate;
-
+////月数组
+//
+//@property(strong,nonatomic) NSMutableArray *monthArray;
+//
+////天数组
+//@property(strong,nonatomic) NSMutableArray *dayArray;
+//
+//
+////周和天
+//@property(strong,nonatomic) NSMutableArray *dayWeeksArray;
+//
+//
+////小时的数组
+//@property(strong,nonatomic) NSMutableArray *hourArray;
+//
+////分钟的数组
+//@property(strong,nonatomic) NSMutableArray *minuteArray;
+//
+//@property(strong,nonatomic) ArchivesModel* achiverDate;
+//
 
 @property (nonatomic, strong)ZKSegment *zkSegment1;
 @property (strong, nonatomic)  UIView  *pHeaderView;
@@ -85,49 +90,49 @@
 
 @implementation ViewController
 
-//周和天匹配
--(void)timeInit
-{
-    
-    _dayWeeksArray=[ArchivesModel ishaves:_dayWeeksArray];
-    _dayArray=[ArchivesModel ishaves:_dayArray];
-    
-    for (int i=0; i<[[self.achiverDate DaysfromYear:[[_yearArray objectAtIndex:_yearIndex] intValue] andMonth:[[[self.achiverDate  monthArrays] objectAtIndex:_monthIndex] intValue]]count]; i++) {
-        
-        NSString *dayStr=[[self.achiverDate DaysfromYear:[[_yearArray objectAtIndex:_yearIndex] intValue] andMonth:[[[self.achiverDate  monthArrays] objectAtIndex:_monthIndex] intValue]]objectAtIndex:i];
-        
-        //周
-        NSString *zhouStr=[self.achiverDate getWeekDayWithYear:[NSString stringWithFormat:@"%@",[_yearArray objectAtIndex:_yearIndex]] month:[NSString stringWithFormat:@"%@",[[self.achiverDate  monthArrays] objectAtIndex:_monthIndex]] day:dayStr];
-        
-        
-        if (_dayIndex==i) {
-            
-            zhouStr=@"今天";
-            
-        }
-        
-        NSString *yue;
-        if ([[_monthArray objectAtIndex:_monthIndex] integerValue]<10) {
-            yue  =[NSString stringWithFormat:@"0%@",[_monthArray objectAtIndex:_monthIndex]];
-            
-        }else{
-            
-            yue  =[NSString stringWithFormat:@"%@",[_monthArray objectAtIndex:_monthIndex]];
-            
-        }
-        
-        
-        [_dayWeeksArray addObject:[NSString stringWithFormat:@"%@\n%@/%@",zhouStr,yue,dayStr]];
-        
-        
-        NSString *dayStr1=[[self.achiverDate DaysfromYear:[[_yearArray objectAtIndex:_yearIndex] intValue] andMonth:[[[self.achiverDate  monthArrays] objectAtIndex:_monthIndex] intValue]]objectAtIndex:i];
-        
-        [_dayArray addObject:[NSString stringWithFormat:@"%@",dayStr1]];
-        
-    }
-    
-    
-}
+////周和天匹配
+//-(void)timeInit
+//{
+//    
+//    _dayWeeksArray=[ArchivesModel ishaves:_dayWeeksArray];
+//    _dayArray=[ArchivesModel ishaves:_dayArray];
+//    
+//    for (int i=0; i<[[self.achiverDate DaysfromYear:[[_yearArray objectAtIndex:_yearIndex] intValue] andMonth:[[[self.achiverDate  monthArrays] objectAtIndex:_monthIndex] intValue]]count]; i++) {
+//        
+//        NSString *dayStr=[[self.achiverDate DaysfromYear:[[_yearArray objectAtIndex:_yearIndex] intValue] andMonth:[[[self.achiverDate  monthArrays] objectAtIndex:_monthIndex] intValue]]objectAtIndex:i];
+//        
+//        //周
+//        NSString *zhouStr=[self.achiverDate getWeekDayWithYear:[NSString stringWithFormat:@"%@",[_yearArray objectAtIndex:_yearIndex]] month:[NSString stringWithFormat:@"%@",[[self.achiverDate  monthArrays] objectAtIndex:_monthIndex]] day:dayStr];
+//        
+//        
+//        if (_dayIndex==i) {
+//            
+//            zhouStr=@"今天";
+//            
+//        }
+//        
+//        NSString *yue;
+//        if ([[_monthArray objectAtIndex:_monthIndex] integerValue]<10) {
+//            yue  =[NSString stringWithFormat:@"0%@",[_monthArray objectAtIndex:_monthIndex]];
+//            
+//        }else{
+//            
+//            yue  =[NSString stringWithFormat:@"%@",[_monthArray objectAtIndex:_monthIndex]];
+//            
+//        }
+//        
+//        
+//        [_dayWeeksArray addObject:[NSString stringWithFormat:@"%@\n%@/%@",zhouStr,yue,dayStr]];
+//        
+//        
+//        NSString *dayStr1=[[self.achiverDate DaysfromYear:[[_yearArray objectAtIndex:_yearIndex] intValue] andMonth:[[[self.achiverDate  monthArrays] objectAtIndex:_monthIndex] intValue]]objectAtIndex:i];
+//        
+//        [_dayArray addObject:[NSString stringWithFormat:@"%@",dayStr1]];
+//        
+//    }
+//    
+//    
+//}
 //天
 - (void)resetSegment1 {
     if (self.zkSegment1) {
@@ -155,14 +160,14 @@
     self.zkSegment1.backgroundColor=[UIColor clearColor];
     self.zkSegment1.zk_itemDefaultColor = [UIColor colorWithRed:250 / 255.0 green:250 / 255.0 blue:250 / 255.0 alpha:0.4];
     self.zkSegment1.zk_itemSelectedColor = [UIColor colorWithRed:250 / 255.0 green:250 / 255.0 blue:250 / 255.0 alpha:1];
-    [self dayArrayItem];
+//    [self dayArrayItem];
     
     
     
-    
-    //将天数装进数组
-    [self.zkSegment1 zk_setItems:_dayWeeksArray];
-    //时间
+//    
+//    //将天数装进数组
+    [self.zkSegment1 zk_setItems:self.dataArray];
+//    //时间
     [self.zkSegment1 zk_itemClickByIndex:_dayIndex];
     
     
@@ -203,14 +208,14 @@
     //    //时间
     //    [self.zkSegment1 zk_itemClickByIndex:_dayIndex];
     
-    //    _dayIndex=itemIndex;
+//        _dayIndex=itemIndex;
     //将天数装进数组
     
     
     _dayIndex=itemIndex;
     
     
-    [self dayArrayItem];
+//    [self dayArrayItem];
     
     
     
@@ -222,37 +227,37 @@
     
 }
 
--(NSString *)theCurrentDate{
+//-(NSString *)theCurrentDate{
+
+//    
+//    if ([_dayArray count]<=_dayIndex) {
+//        
+//        _dayIndex=[_dayArray count];
+//    }
+//    
+//    
+//    NSString *dateStr=[NSString stringWithFormat:@"%@-%@-%@ ",[_yearArray objectAtIndex:_yearIndex],[_monthArray1 objectAtIndex:_monthIndex],[_dayArray objectAtIndex:_dayIndex]];
+//    
     
-    
-    if ([_dayArray count]<=_dayIndex) {
-        
-        _dayIndex=[_dayArray count];
-    }
-    
-    
-    NSString *dateStr=[NSString stringWithFormat:@"%@-%@-%@ ",[_yearArray objectAtIndex:_yearIndex],[_monthArray1 objectAtIndex:_monthIndex],[_dayArray objectAtIndex:_dayIndex]];
-    
-    
-    
-    NSLog(@"dateStr-------%@",dateStr);
-    return    dateStr;
-    
-    
-    
-    
-    
-}
+//    
+//    NSLog(@"dateStr-------%@",dateStr);
+//    return    dateStr;
+//    
+//    
+//    
+//    
+//    
+//}
 
 
--(void)dayArrayItem{
-    
-    if ([_dayArray count]<=_dayIndex) {
-        
-        _dayIndex=[_dayArray count]-1;
-    }
-    
-}
+//-(void)dayArrayItem{
+//    
+//    if ([_dayArray count]<=_dayIndex) {
+//        
+//        _dayIndex=[_dayArray count]-1;
+//    }
+//    
+//}
 
 
 
@@ -281,60 +286,100 @@
         
     }
     
-    //    self.aTableView.frame=CGRectMake(0, heightSegment, kDeviceWidth, kDeviceHeight);
+    
+    self.dataArray = [NSMutableArray array];
     
     
-    //
-    self.achiverDate=[ArchivesModel  getArchivesDataSource];
     
-    _yearArray= [self.achiverDate  yearArrayss];
-    
-    //年
-    
-    _yearIndex=[[[self.achiverDate indexOfTheCurrentTime]objectAtIndex:0] intValue];
-    //月
-    _monthIndex= [[[self.achiverDate indexOfTheCurrentTime]objectAtIndex:1] intValue];
-    
-    //日
-    _dayIndex=[[[self.achiverDate indexOfTheCurrentTime]objectAtIndex:2] intValue];
-    
-    //月
-    _monthArray=[ArchivesModel ishaves:_monthArray];
-    
-    
-    //月的数组
-    for (int i=0; i<[[self.achiverDate  monthArrays]count]; i++) {
-        [_monthArray addObject:[NSString stringWithFormat:@"%@",[[self.achiverDate  monthArrays]objectAtIndex:i]]];
+    //前推3周，21天日期数据集合
+    NSMutableArray *forward = [self getForwardDataArr:22];
+    for (NSInteger i=0; i<forward.count; i++) {
+        [self.dataArray addObject:forward[i]];
     }
     
     
-    _monthArray1=[ArchivesModel ishaves:_monthArray1];
-    //月的数组
-    for (int i=0; i<[[self.achiverDate  monthArrays]count]; i++) {
+    //当前日期数据集合
+    NSMutableArray *current = [self getForwardDataArr:0];
+    for (NSInteger i=0; i<current.count; i++) {
+        //        [dataArray addObject:current[i]];
         
-        if (i>8) {
-            [_monthArray1 addObject:[NSString stringWithFormat:@"%@",[[self.achiverDate  monthArrays]objectAtIndex:i]]];
+        NSLog(@"22222----%@",[self getYearMonthDayWeekWithDate:[NSDate date]]);
+        
+        if ([[self getYearMonthDayWeekWithDate:[NSDate date]] isEqualToString:current[i]]) {
             
-        }else{
-            [_monthArray1 addObject:[NSString stringWithFormat:@"0%@",[[self.achiverDate  monthArrays]objectAtIndex:i]]];
+            NSDateFormatter  *dateformatter=[[NSDateFormatter alloc] init];
+            //    [dateformatter setDateFormat:@"yyy"];
+            //    NSString *  yearString = [dateformatter stringFromDate:date];
+            [dateformatter setDateFormat:@"MM"];
+            NSString *  monthString = [dateformatter stringFromDate:[NSDate date]];
+            [dateformatter setDateFormat:@"dd"];
+            NSString *  dayString = [dateformatter stringFromDate:[NSDate date]];
+            [dateformatter setDateFormat:@"EEE"];
+            [dateformatter setLocale:[[NSLocale alloc]initWithLocaleIdentifier:@"zh_CN"]];
+//            NSString *weekString = [dateformatter stringFromDate:[NSDate date]];
+            
+            //    return [NSString stringWithFormat:@"%@-%@-%@",monthString,dayString,weekString];
+            
+            
+            
+            [self.dataArray addObject:[NSString stringWithFormat:@"%@\n%@/%@",@"今天",monthString,dayString]];
+
             
         }
+        
+    }
+
+    
+    //后推一周，7天日期数据集合
+    NSMutableArray *backward = [self getBackwardDataArr:7];
+    for (NSInteger i=0; i<backward.count; i++) {
+        [self.dataArray addObject:backward[i]];
+    }
+    
+    for (int i=0; i<self.dataArray.count; i++) {
+        NSLog(@"%@",self.dataArray[i]);
+        //正数22，倒数8为今天日期
+    }
+
+    
+    
+    
+    
+    self.YearMonthDayArray=[NSMutableArray array];
+
+    
+    
+    //前推3周，21天日期数据集合
+    NSMutableArray *forwardYearMonthDay = [self getForwardYearMonthDayDataArr:22];
+    for (NSInteger i=0; i<forwardYearMonthDay.count; i++) {
+        [self.YearMonthDayArray addObject:forwardYearMonthDay[i]];
+    }
+
+    
+    //当前日期数据集合
+    NSMutableArray *currentYearMonthDay = [self getForwardYearMonthDayDataArr:0];
+    for (NSInteger i=0; i<currentYearMonthDay.count; i++) {
+                [self.YearMonthDayArray addObject:currentYearMonthDay[i]];
+//        [self.dataArray addObject:[NSString stringWithFormat:@"%@%@",@"%@",currentYearMonthDay[i]]];
+        
+    }
+
+    
+    //后推一周，7天日期数据集合
+    NSMutableArray *backwardYearMonthDay = [self getYearMonthDayDataArr:7];
+    for (NSInteger i=0; i<backwardYearMonthDay.count; i++) {
+        [self.YearMonthDayArray addObject:backwardYearMonthDay[i]];
     }
     
     
+    for (int i=0; i<self.YearMonthDayArray.count; i++) {
+        NSLog(@"YearMonthDayArray---%@",self.YearMonthDayArray[i]);
+        //正数22，倒数8为今天日期
+    }
     
     
-    
-    
-    [self timeInit];
-    
-    
-    
-    //小时的数组
-    _hourArray=   [self.achiverDate  hourArrays];
-    //分
-    _minuteArray=  [self.achiverDate  minuteArrays];
-    
+
+    _dayIndex=22;
     
     self.zkSegmentStyle = ZKSegmentRectangleStyle;
     
@@ -342,6 +387,151 @@
     [self resetSegment1];
 
     
+    NSLog(@"%lu",(unsigned long)self.dataArray.count);
+    
+}
+
+
+//获取几天后的日期集合
+- (NSMutableArray *)getYearMonthDayDataArr:(NSInteger)index
+{
+    NSMutableArray *array = [NSMutableArray array];
+    if (index>0) {
+        for (NSInteger i=0; i<index; i++) {
+            NSDate *date = [self getOneDay:i+1];
+            NSString *ymdString = [self getYearMonthDayWithDate:date];
+            [array addObject:ymdString];
+        }
+    }else{
+        NSDate *date = [self getOneDay:index];
+        NSString *ymdString = [self getYearMonthDayWithDate:date];
+        [array addObject:ymdString];
+    }
+    
+    return array;
+}
+
+//获取几天前的日期集合
+- (NSMutableArray *)getForwardYearMonthDayDataArr:(NSInteger)index
+{
+    NSMutableArray *array = [NSMutableArray array];
+    if (index>0) {
+        for (NSInteger i=index; i>0; i--) {
+            NSDate *date = [self getOneDay:-i];
+            NSString *ymdString = [self getYearMonthDayWithDate:date];
+            [array addObject:ymdString];
+        }
+    }else{
+        NSDate *date = [self getOneDay:index];
+        NSString *ymdString = [self getYearMonthDayWithDate:date];
+        [array addObject:ymdString];
+    }
+    
+    return array;
+}
+
+//根据日期获取年月日周
+- (NSString *)getYearMonthDayWithDate:(NSDate *)date
+{
+    NSDateFormatter  *dateformatter=[[NSDateFormatter alloc] init];
+    [dateformatter setDateFormat:@"yyy"];
+    NSString *  yearString = [dateformatter stringFromDate:date];
+    [dateformatter setDateFormat:@"MM"];
+    NSString *  monthString = [dateformatter stringFromDate:date];
+    [dateformatter setDateFormat:@"dd"];
+    NSString *  dayString = [dateformatter stringFromDate:date];
+    //    [dateformatter setDateFormat:@"EEE"];
+    [dateformatter setLocale:[[NSLocale alloc]initWithLocaleIdentifier:@"zh_CN"]];
+    //    NSString *weekString = [dateformatter stringFromDate:date];
+    
+    //    return [NSString stringWithFormat:@"%@-%@-%@",monthString,dayString,weekString];
+    
+    
+    //    return [NSString stringWithFormat:@"%@\n%@/%@",weekString,monthString,dayString];
+    
+    return [NSString stringWithFormat:@"%@-%@-%@",yearString,monthString,dayString];
+}
+
+//获取几天后的日期集合
+- (NSMutableArray *)getBackwardDataArr:(NSInteger)index
+{
+    NSMutableArray *array = [NSMutableArray array];
+    if (index>0) {
+        for (NSInteger i=0; i<index; i++) {
+            NSDate *date = [self getOneDay:i+1];
+            NSString *ymdString = [self getYearMonthDayWeekWithDate:date];
+            [array addObject:ymdString];
+        }
+    }else{
+        NSDate *date = [self getOneDay:index];
+        NSString *ymdString = [self getYearMonthDayWeekWithDate:date];
+        [array addObject:ymdString];
+    }
+    
+    return array;
+}
+
+
+
+//获取几天前的日期集合
+- (NSMutableArray *)getForwardDataArr:(NSInteger)index
+{
+    NSMutableArray *array = [NSMutableArray array];
+    if (index>0) {
+        for (NSInteger i=index; i>0; i--) {
+            NSDate *date = [self getOneDay:-i];
+            NSString *ymdString = [self getYearMonthDayWeekWithDate:date];
+            [array addObject:ymdString];
+        }
+    }else{
+        NSDate *date = [self getOneDay:index];
+        NSString *ymdString = [self getYearMonthDayWeekWithDate:date];
+        [array addObject:ymdString];
+    }
+    
+    return array;
+}
+
+
+
+
+//根据日期获取年月日周
+- (NSString *)getYearMonthDayWeekWithDate:(NSDate *)date
+{
+    NSDateFormatter  *dateformatter=[[NSDateFormatter alloc] init];
+    //    [dateformatter setDateFormat:@"yyy"];
+    //    NSString *  yearString = [dateformatter stringFromDate:date];
+    [dateformatter setDateFormat:@"MM"];
+    NSString *  monthString = [dateformatter stringFromDate:date];
+    [dateformatter setDateFormat:@"dd"];
+    NSString *  dayString = [dateformatter stringFromDate:date];
+    [dateformatter setDateFormat:@"EEE"];
+    [dateformatter setLocale:[[NSLocale alloc]initWithLocaleIdentifier:@"zh_CN"]];
+    NSString *weekString = [dateformatter stringFromDate:date];
+    
+    //    return [NSString stringWithFormat:@"%@-%@-%@",monthString,dayString,weekString];
+    
+    
+    return [NSString stringWithFormat:@"%@\n%@/%@",weekString,monthString,dayString];
+    
+    //    return [NSString stringWithFormat:@"%@-%@-%@-%@",yearString,monthString,dayString,weekString];
+}
+
+/**
+ *  获取几年几月几日后的日期，0表示当天，负数表示之前
+ *  这里只要取到日就好了，年月置0，表示当年当月
+ */
+- (NSDate *)getOneDay:(NSInteger)day {
+    NSInteger year = 0, month = 0;
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *comps = nil;
+    comps = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitWeekday fromDate:[NSDate date]];
+    NSDateComponents *adcomps = [[NSDateComponents alloc] init];
+    [adcomps setYear:year];
+    [adcomps setMonth:month];
+    [adcomps setDay:day];
+    NSDate *newdate = [calendar dateByAddingComponents:adcomps toDate:[NSDate date] options:0];
+    return newdate;
 }
 
 
